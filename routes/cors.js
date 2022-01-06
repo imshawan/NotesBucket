4807d1f15fb8631b3a876668a5eb442d040f3d67
Shawan Mandal <shawan.sm049@gmail.com>
2022-01-07 01:03:58 +0530

    removed origin loggings

--- routes/cors.js
+++ routes/cors.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const whitelist = ['http://localhost:3000', 'https://localhost:3000'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
-    console.log(req.header('Origin'));
+    //console.log(req.header('Origin'));
     if(whitelist.indexOf(req.header('Origin')) !== -1) {
         corsOptions = { origin: true };
     }
